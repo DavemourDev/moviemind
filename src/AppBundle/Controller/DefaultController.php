615e3a2567429bcbd461a77a70ccc3ea7c9f298e
A4Alumno <A4Alumno@PCAULA151.aules.local>
2017-05-03 14:08:30 +0200

    Nueva ruta de Busqueda y Pagina de Resultados

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,22 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        
+        
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Route("/buscar/{busqueda}", name="busqueda")
+     */
+    public function buscarAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('navigation/busqueda.html.twig', [
+            'busqueda'=>$request->get('busqueda'),
         ]);
     }
 }
