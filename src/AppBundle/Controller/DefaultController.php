d22e2ccc4af0227522df24ee8cb9a58fb58a4671
A4Alumno <A4Alumno@PCAULA166.aules.local>
2017-05-10 14:33:08 +0200

    login

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Pelicula;
+use AppBundle\Utils\Imdb;
+
 
 
 class DefaultController extends Controller {
@@ -23,7 +25,7 @@ class DefaultController extends Controller {
     }
 
     /**
-     * @Route("/buscar/", name="resultado")
+     * @Route("/buscar/{texto}", name="resultado")
      */
     public function buscarAction(Request $request) {
         //$busqueda = $request->get("buscar");
@@ -40,9 +42,7 @@ class DefaultController extends Controller {
             $r->setSource('Omdb');
             $r->fetch();
         }
-        
-        //var_dump($resultados[3]->getInfo('Title'));
-        
+
         //$peliculas=[];
         
 //        foreach($resultados as $r)
@@ -58,5 +58,97 @@ class DefaultController extends Controller {
             ]);
         
     }
+    
+    /**
+     * @Route("/info/{id}", name="products")
+     */
+    public function productsAction($id, Request $request) 
+    {
+        
+        
+    }
+    
+    
+    
+    /**
+     * @Route("/info/", name="info")
+     */
+    public function infoAction(Request $request) {
+        //$busqueda = $request->get("buscar");
 
+        $productos = $this->getDoctrine()
+                ->getRepository('AppBundle:Producto')
+                ->findAll();
+                //->find($busqueda);
+        
+        $peliculas= $this->getDoctrine()
+                ->getRepository('AppBundle:Pelicula')
+                ->findAll();
+        
+        $ediciones= $this->getDoctrine()
+                ->getRepository('AppBundle:Edicion')
+                ->findAll();
+        
+        foreach($peliculas as $p)
+        {
+            $p->setSource('Omdb');
+            $p->fetch();
+        }
+
+        return $this->render('full-views/test.html.twig', [
+                    'productos' => $productos,
+                    'peliculas' => $peliculas,
+                    'ediciones' => $ediciones,
+        ]);
+        
+    }
+
+    
+    /**
+     * @Route("/login-register/", name="login-register")
+     */
+    public function loginRegisterAction(Request $request) {
+    
+        return $this->render('full-views/login-register.html.twig', [
+            'error'=>false,
+            'notice'=>''
+        ]);
+        
+    }
+    
+    /**
+     * @Route("/register/", name="register")
+     */
+    public function registerAction(Request $request)
+    {
+        $notice='';
+        
+        $user=$request->get('user');
+        $email=$request->get('email');
+        $password=$request->get('password');
+        $cpassword=$request->get('password-confirm');
+        
+        if(empty($user) || empty($email) || empty($password) || empty($cpassword))
+        {
+            return $this->render("full-views/login-register.html.twig",[
+                'notice'=>'No has completado todos los campos...',
+                'error'=>true
+            ]);
+        }
+            
+        if(! ($cpassword == $password))
+        {
+            return $this->render("full-views/login-register.html.twig",[
+                'notice'=>'La contraseña no coincide con su confirmación',
+                'error'=>true
+            ]);
+        }
+        
+        
+        return $this->render("full-views/test.html.twig",[
+            'user'=>$user,
+            'notice'=>$notice
+        ]);
+    }
+    
 }
